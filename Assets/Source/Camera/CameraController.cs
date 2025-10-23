49fbb71d6f793ae62dd9cb8febe9fb413330749f
Gorka Urrutia del Pico <gorka_urrutiadelpico@euneiz.com>
2025-10-23 18:15:35 +0200

    wip: camera follows smoothly

--- Assets/Source/Camera/CameraController.cs
+++ Assets/Source/Camera/CameraController.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private Transform target;
+
+    private Vector3 offSet = new Vector3(0, 5, 0);
+    public float smoothTime = 0.5f;
+
+    Vector3 currentVelocity;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void LateUpdate()
+    {
+        //transform.position = target.position +  offSet;
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            target.position + offSet,
+            ref currentVelocity,
+            smoothTime
+        );
+    }
+
+
 }
