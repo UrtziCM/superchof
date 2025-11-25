a11313a4b9d52363f03be6d818f5e3eefead7c8c
ImanolRelloso <imanol_relloso@euneiz.com>
2025-11-25 12:09:43 +0100

    Add: DeathMenu

---
+++ Assets/Source/SceneManager/DeathMenu/DeathMenu.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+public class DeathMenu : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text maxScoreText;
+
+    [SerializeField]
+    private TMP_Text scoreText;
+
+    ViewSceneManager viewSceneManager;
+    private void Awake()
+    {
+        viewSceneManager = GameObject.Find("SceneManager").GetComponent<ViewSceneManager>();
+    }
+    private void Start()
+    {
+        Time.timeScale = 0.0f;
+
+        maxScoreText.text = "Max Score: " + GameManager.Instance.maxSaveScore.ToString();
+
+        scoreText.text = "Score: " + GameManager.Instance.scoreManagerInstance;
+    }
+
+    public void Replay()
+    {
+        viewSceneManager.UnloadScene("DeathMenu");
+        Time.timeScale = 1.0f;
+    }
+}
